fce16fbf56d3665f159981cb7b0edc7faa3ad76e
Victor <victor.kryukov@gmail.com>
2020-06-17 00:38:00 -0700

    Move and rename backup files

--- roam_backup.py
+++ roam_backup.py
@@ -2,11 +2,11 @@
 
 import getpass
 import glob
+import os.path
 import tempfile
 import time
 import shutil
 import sys
-import subprocess
 
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
@@ -14,7 +14,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-#https://stackoverflow.com/questions/48263317/selenium-python-waiting-for-a-download-process-to-complete-using-chrome-web
+# https://stackoverflow.com/questions/48263317/selenium-python-waiting-for-a-download-process-to-complete-using-chrome-web
 def every_downloads_chrome(driver):
     if not driver.current_url.startswith("chrome://downloads"):
         driver.get("chrome://downloads/")
@@ -46,22 +46,7 @@ def download_local_graph(driver, name):
         time.sleep(0.1)
         el.click()
 
-    # time.sleep(0.1)
-    # el = driver.find_element_by_xpath("//div[contains(text(), 'Export All')]")
-    # time.sleep(0.1)
-    # el.click()
-    #
-    # for css_selector in (
-    #         ".bp3-button-text",
-    #         ".bp3-text-overflow-ellipsis",
-    #         ".bp3-intent-primary"
-    # ):
-    #     el = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.CSS_SELECTOR, css_selector))
-    #     el.click()
-
-    # Give it some time to finish the download
     time.sleep(3)
-    # WebDriverWait(driver, 10).until(every_downloads_chrome)
     print("done.")
 
 
@@ -77,10 +62,23 @@ def clone_chrome_user_data(username, dst_dir):
     print("done.")
 
 
+def move_roam_exports_since(start, backup_dir, username, local_graph):
+    """Move all Roam-Export-*.zip files created after start into dst_dir"""
+    paths = [p for p in glob.glob(f"/Users/{username}/Downloads/Roam-Export-*.zip")
+             if os.path.getctime(p) >= start]
+    for p in paths:
+        base = os.path.splitext(os.path.basename(p))[0][len("Roam-Export-"):]
+        fmt_time = time.strftime("%Y-%m-%d-%H%M%S", time.localtime(float(base)/1000))
+        shutil.move(p, os.path.join(backup_dir, f"{local_graph}-{fmt_time}.zip"))
+
+
 if __name__ == "__main__":
     username = getpass.getuser()
     tmp = tempfile.TemporaryDirectory()
     user_data_dir = tmp.name + "/chrome"
+
+    local_graph = len(sys.argv) > 1 and sys.argv[1] or "test"
+    backup_dir = len(sys.argv) > 2 and sys.argv[2] or "/tmp/a"
 
     clone_chrome_user_data(username, user_data_dir)
 
@@ -89,6 +87,8 @@ if __name__ == "__main__":
     chrome_options.add_argument("--profile-directory=Default")
     driver = Chrome(options=chrome_options)
 
-    download_local_graph(driver, sys.argv[1])
+    start = time.time()
+    download_local_graph(driver, local_graph)
+    move_roam_exports_since(start, backup_dir, username, local_graph)
 
     driver.quit()
